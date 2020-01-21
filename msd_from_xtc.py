64fff17abbe751bef368e5be18fa55bc93ef118c
Shea Fitzgerald <sheamusfitz@gmail.com>
2020-01-20 19:04:55 -0500

    I can now retrieve plots of CoM position and rotation angle (mostly)

--- msd_from_xtc.py
+++ msd_from_xtc.py
@@ -1,6 +1,6 @@
 
-import sys
-print('Version ',sys.version)
+# import sys
+# print('Version ',sys.version)
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,6 +10,7 @@ import json
 import readline
 from termcolor import cprint
 
+# TODO: make a function that creates a "dry" .gro file
 
 def indexer():
   """
@@ -59,20 +60,48 @@ def msd():
   print(sum(mol_count_dict.values()))
 
   first_atom = 0
+  # print(len(u.trajectory), 'frames')
+  # coms = np.zeros(
+  #   (sum(mol_count_dict[s] for s in mol_count_dict), len(u.trajectory), 3)
+  # )
+  # rotators = np.zeros(
+  #   (sum(mol_count_dict[s] for s in mol_count_dict), len(u.trajectory), 3)
+  # )
   for species in mol_count_dict:
     for molecule in range(mol_count_dict[species]):
       cprint(species+'\t'+str(molecule), attrs=['bold'])
-      # print(u.atoms[tracking_indices[species]['com index']])
-
 
       mol = u.atoms[np.array(tracking_indices[species]['com index'])+first_atom]
-      coms = np.array([(u.trajectory.time, mol.center_of_geometry()) for ts in u.trajectory])
-      print(coms,'\n\n')
-      print(coms[0][0])
-      print(coms[0][1])
+      position_data = np.array([
+        (
+          mol.center_of_geometry(),
+          u.atoms[tracking_indices[species]['rotator index']+first_atom].position
+        ) for ts in u.trajectory], dtype=object)
+      position_data[:,1] -= position_data[:,0]
+      # print(position_data,'\n\n')
+      # print(position_data[:], '\n\n')
+      # print(position_data[:,0,0], '\n\n')
 
-      first_atom += 100 # TODO: this number is garbage
+      thetas = np.array(
+        [
+          (
+            np.arctan2(ts[1,1], ts[1,0])
+          ) for ts in position_data
+        ]
+      )
+      # TODO: this doesn't deal with "jumping over" from -3.14 radians to +3.14 radians
+      # I'm thinking of doing some sort of loop that checks each step and goes "is theta_i+1 - theta_i > pi? -> add pi" and vice versa for the difference being less than -pi (or whatever.) I probably have signs wrong in there because math is hard
 
+      plt.plot(thetas*180/np.pi)
+      plt.title("Angle of rotation (degrees)")
+      plt.show()
+
+      first_atom += tracking_indices[species]['length']
+
+  # This is a plot of the motion of the center of mass of the object in question
+  # plt.plot(position_data[:,0,0], position_data[:,0,1])
+  # plt.title("Center of Mass Position")
+  # plt.show()
 
 
 
